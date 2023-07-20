b1c0a564a87e0b0bf4a4ebb0dd6362b6c779e097
asus <asus>
2023-07-20 22:57:09 +0500

    model with issue

--- mahalla/models.py
+++ mahalla/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Sector(models.Model):
+    name = models.CharField(verbose_name="sektor nomi", max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "sektor"
+        ordering = ("id",)
+
+
+class Neighborhood(models.Model):
+    sector = models.ForeignKey('sector', on_delete=models.PROTECT,
+                               help_text="Mahalla qaysi sektorga tegishli ekanligini tanlang")
+    name = models.CharField(verbose_name="mahalla nomi ", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Mahalla"
+        verbose_name_plural = "Mahalalar"
+        ordering = ("id",)
+
+
+class Citizen(models.Model):
+    GENDER = (
+        ("male", "erkak"),
+        ("female", "ayol"),
+    )
+    EDUCATED = (
+        ("middle", "o'rta ma'lumotli"),
+        ("senior", "oliy malumotli"),
+    )
+    IRON_NOTE = (
+        ("yes", "ha"),
+        ("no", "yo'q")
+    )
+    ABROAD = (
+        ("yes", "ha"),
+        ("no", "yo'q"),
+    )
+    DISABILLITY = (
+        ("yes", "ha"),
+        ("no", "yo'q")
+    )
+    WOMEN_NOTE = (
+        ("yes", "ha"),
+        ("no", "yo'q")
+    )
+    neighborhood = models.ForeignKey("neighborhood", verbose_name="mahallani tanlang",
+                                     on_delete=models.PROTECT)
+    full_name = models.CharField(verbose_name="F.I.O", max_length=25)
+    passport = models.CharField(verbose_name="Passport seriyasi", max_length=20)
+    birthdate = models.DateField(verbose_name="tug'ilgan sanasi")
+    gender = models.CharField(
+        max_length=250,
+        choices=GENDER,
+        verbose_name="Fuqoro jinsi",
+    )
+    educated = models.CharField(
+        verbose_name="Fuqoro ma'lumoti",
+        choices=EDUCATED,
+        max_length=250
+    )
+    adress = models.TextField(verbose_name="yashash manzili")
+
+    iron_note = models.CharField(
+        verbose_name="Temir daftarga kiritilganmi?",
+        max_length=250,
+        choices=IRON_NOTE
+    )
+    abroad = models.CharField(
+        verbose_name="Chet eldami",
+        max_length=250,
+        choices=ABROAD,
+
+    )
+    disabillity = models.CharField(
+        verbose_name="Nogironligi bormi?",
+        max_length=20,
+        choices=DISABILLITY,
+    )
+    women_note = models.CharField(
+        max_length=250,
+        verbose_name="Ayollar daftariga kiritilganmi?",
+        choices=WOMEN_NOTE
+    )
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = "Fuqoro"
+        verbose_name_plural = "Fuqorolar"
+        ordering = ("id",)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+
+class MyUserManager(BaseUserManager):
+
+
+    def create_user(self, username, password=None, **extra_fields):c
+        user = self.model(username, **extra_fields)
+        user.set_password(password)
+        user.is_active = True
+        user.save()
+        return user
+
+
+    def _create_user(self, username, password=None, **extra_fields):
+        user = self.model(username=username, **extra_fields)
+        user.is_active = True
+        user.set_password(password)
+        user.save()
+        return user
+
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser need to have is_staff=True.')
+        if extra_fields.get('is_suoeruser'):
+            raise ValueError('Supeeruser need to have is_superuser=True.')
+        return self._create_user(username, password, **extra_fields)
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE = (
+        ('sector_leader', 'Sektor rajbari'),
+        ('nighborhood_leader', 'Mahalla rahbari'),
+        ('measurs', "Hokim"),
+        ('admin', 'Admin'),
+    )
+    first_name = models.CharField(verbose_name='Ism', max_length=250, blank=True)
+    last_name = models.CharField(verbose_name="Familiya", max_length=250, blank=True)
+    email = models.EmailField(verbose_name="Pochta", max_length=250, blank=True)
+    username = models.CharField(verbose_name='username', max_length=250, unique=True)
+    is_staff = models.BooleanField(verbose_name='Hodimlarning holati', default=False)
+    is_active = models.BooleanField(verbose_name="Faol", default=True)
+    birthdate = models.DateField(verbose_name="Tug'ilgan sanasi",
+                                 null=True, blank=True)
+    phone = models.CharField(
+        verbose_name="Telefon raqami",
+        max_length=250,
+        null=True,
+        blank=True
+    )
+    user_type = models.CharField(
+        verbose_name="Foydalanuvchi turi",
+        max_length=250,
+        choices=USER_TYPE,
+        default="nighborhood_leader"
+    )
+    neighborhood = models.ForeignKey("mahalla.Neighborhood", verbose_name="qaysi mahalla rahbari",
+                                     on_delete=models.SET_NULL, null=True, blank=True)
+    sector = models.ForeignKey("mahalla.Sector", verbose_name='Qaysi sektor rahbari?',
+                               on_delete=models.SET_NULL, null=True, blank=True)
+
+
+
+    USER_FIELD = 'username'
+    objects = MyUserManager()
+
+
+    def __str__(self):
+        return self.username
+
+
+    def get_full_name(self):
+        return f"ism {self.first_name} familiya {self.last_name}"
+
+
+    def get_short_name(self):
+        return self.first_name
+
+
+    class Meta:
+        verbose_name = "Foydalanuvchi"
+        verbose_name_plural = "Fydalanuvchilar"
+
+
+
+
+
+
+
+
